6166aa19e640c5bb23c5968c0715cdb307dd7f33
David <davidvo1992@gmail.com>
2023-02-19 15:58:02 -0600

    commit tree 9a21dae19af9fc2416aaa9156a3248401700df3d

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Lebron James",
+
+  streetAddress: "4344 Malibu lane",
+};
+
+function updateEmployeeWithKeyAndValue(newEmployee, key, value) {
+  const copyOfEmployee = { ...newEmployee };
+
+  copyOfEmployee[key] = value;
+
+  return copyOfEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(newEmployee, key, value) {
+  newEmployee[key] = value;
+
+  return newEmployee;
+}
+
+function deleteFromEmployeeByKey(newEmployee, key) {
+  const copyOfEmployee = { ...newEmployee };
+
+  delete copyOfEmployee[key];
+
+  return copyOfEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(newEmployee, key) {
+  delete newEmployee[key];
+
+  return newEmployee;
+}
+
+//Shallow copy is pointing to original
+//Deep copy is it's own thing - "New array created"
